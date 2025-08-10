74d69a8d8fb0a99b3380779fd27b7aaef6f347f1
tamilarasi977 <contact.tamil2@gmail.com>
2025-08-10 14:41:13 +0530

    react file added

---
+++ App.js
@@ -0,0 +1,44 @@
+
+import React from 'react';
+// import './App.css';
+//import Listuser from './List Adding and Deleting/List crud';
+import Tableuser from './Table Adding and Deleting/Table crud';
+// import FormAlertValidationuser from "./Form Alert Validation/Form Alert validationcrud";
+//import FormValidationuser from "./FormValidation/Formvalidationcrud";
+//import Fetchingdata from './Axios and Fetch/Fetch data';
+// import Loginform from "./Routers/Logform";
+//  import Regform from "./Routers/RegForm";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+//  import Parenskills from "./Data Passing/Parent";
+//  import  ChildSkills from "./Data Passing/Child";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Tableuser/>
+    </div>
+  );
+}
+
+export default App;
+
+// {/* <Tableuser/> */}
+//  <ChildSkills/>
+// {/* <Parenskills/>  */}
+// {/* <Loginform/>
+// <Regform/> */}
+
+//       {/* <BrowserRouter>
+//         <Routes>
+//           <Route path="/" element={<Loginform />} /><Route />
+//           <Route path="/RegForm" element={<Regform />} /><Route />
+//           <Route path="/LogForm" element={<Loginform />} /><Route />
+//         </Routes>
+//       </BrowserRouter> */}
+      
+// {/* <Fetchingdata/> */ }
+// {/* <Listuser/> */ }
+// {/* <Tableuser/> */ }
+// {/* <FormAlertValidationuser/> */ }
+// {/* <FormValidationuser/> */ }
